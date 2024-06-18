097c9fce05eddeb772e0ef543ed1caeccf8ae89b
ushinohama966 <ushinohama966@gmail.com>
2024-06-18 22:44:03 +0900

    feat: array function
    add following functions:
    
    - map + curried map
    - reduce + curried reduce
    - curried filter

--- v1/array/array.go
+++ v1/array/array.go
@@ -1,8 +1,10 @@
 package array
 
-func Filter(fn func(elem any, arr []any) bool, arr []any) []any {
+type Generics any
+
+func Filter[T Generics](fn func(elem T, arr []T) bool, arr []T) []T {
 	if len(arr) == 0 {
-		return []any{}
+		return []T{}
 	}
 
 	head := arr[0]
@@ -10,10 +12,72 @@ func Filter(fn func(elem any, arr []any) bool, arr []any) []any {
 	next := Filter(fn, arr[1:])
 
 	if fn(head, arr) {
-		newArr := []any{head}
+		newArr := []T{head}
 		newArr = append(newArr, next...)
 		return newArr
 	}
 
 	return next
+}
+
+func FilterCurry[T Generics](fn func(elem T) bool) func(arr []T) []T {
+	return func(arr []T) []T {
+		var res []T
+
+		for _, v := range arr {
+			if fn(v) {
+				res = append(res, v)
+			}
+		}
+
+		return res
+	}
+}
+
+func Map[T Generics](fn func(v T) T, arr []T) []T {
+	if len(arr) == 0 {
+		return []T{}
+	}
+
+	head := fn(arr[0])
+
+	next := Map(fn, arr[1:])
+
+	newArr := []T{head}
+	newArr = append(newArr, next...)
+
+	return newArr
+}
+
+func MapCurry[T Generics](fn func(v T) T) func(arr []T) []T {
+	return func(arr []T) []T {
+		var res []T
+
+		for _, v := range arr {
+			res = append(res, fn(v))
+		}
+
+		return res
+	}
+}
+
+func Reduce[T Generics](fn func(acc, cur T) T, arr []T, initial T) T {
+	res := initial
+	for _, v := range arr {
+		res = fn(res, v)
+	}
+
+	return res
+}
+
+func ReduceCurry[T Generics](fn func(acc, cur T) T) func(arr []T, initial T) T {
+	return func(arr []T, initial T) T {
+		res := initial
+
+		for _, v := range arr {
+			res = fn(res, v)
+		}
+
+		return res
+	}
 }
--- v1/array/array_test.go
+++ v1/array/array_test.go
@@ -6,33 +6,27 @@ import (
 )
 
 func TestFilter(t *testing.T) {
-	var filterByGreater = func(elem any, arr []any)bool{
-		n, ok := elem.(int)
-		if !ok  {
-			return false
-		}
-
-		return n >= 10
+	var filterByGreater = func(elem int, arr []int) bool {
+		return elem >= 10
 	}
 
-	cases := []struct {
+	type testCase[T Generics] struct {
 		name string
 		arg  struct {
-			fn  func(elem any, arr []any) bool
-			arr []any
+			fn  func(elem T, arr []T) bool
+			arr []T
 		}
-		expect []any
-	}{
+		expect []T
+	}
+
+	cases := []testCase[int]{
 		{
-			name: "",
-			arg: struct {
-				fn  func(elem any, arr []any) bool
-				arr []any
-			}{
-				arr: []any{10, 13, 5, -9},
+			name: "should success",
+			arg: struct{fn func(elem int, arr []int) bool; arr []int}{
 				fn: filterByGreater,
+				arr: []int{10, 3, 13, -5},
 			},
-			expect: []any{10,13},
+			expect: []int{10, 13},
 		},
 	}
 
@@ -40,6 +34,192 @@ func TestFilter(t *testing.T) {
 		t.Run(c.name, func(t *testing.T) {
 			actual := Filter(c.arg.fn, c.arg.arr)
 			if !reflect.DeepEqual(c.expect, actual) {
+				t.Errorf("actual: %v", actual)
+				t.Errorf("expect: %v", c.expect)
+			}
+		})
+	}
+}
+
+func TestFilterCurry(t *testing.T) {
+	var filterByGreater = func(elem int) bool {
+		return elem >= 10
+	}
+
+	type testCase[T Generics] struct {
+		name string
+		arg  struct {
+			fn  func(elem T) bool
+			arr []T
+		}
+		expect []T
+	}
+
+	cases := []testCase[int]{
+		{
+			name: "should success",
+			arg: struct{fn func(elem int) bool; arr []int}{
+				fn: filterByGreater,
+				arr: []int{10, 3, 13, -5},
+			},
+			expect: []int{10, 13},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			filter := FilterCurry(c.arg.fn)
+
+			actual := filter(c.arg.arr)
+			if !reflect.DeepEqual(c.expect, actual) {
+				t.Errorf("actual: %v", actual)
+				t.Errorf("expect: %v", c.expect)
+			}
+		})
+	}
+}
+
+func TestMap(t *testing.T) {
+	type testCase[T any] struct {
+		name string
+		arg  struct {
+			fn  func(v T) T
+			arr []T
+		}
+		expect []T
+	}
+	cases := []testCase[int]{
+		{
+			name: "should success",
+			arg: struct {
+				fn  func(v int) int
+				arr []int
+			}{
+				arr: []int{1, 2, 3, 4, 5},
+				fn:  func(v int) int { return v * 2 },
+			},
+			expect: []int{2, 4, 6, 8, 10},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := Map(c.arg.fn, c.arg.arr)
+
+			if !reflect.DeepEqual(actual, c.expect) {
+				t.Errorf("expect: %v", c.expect)
+				t.Errorf("actual: %v", actual)
+			}
+		})
+	}
+}
+
+func TestMapCurry(t *testing.T) {
+	type testCase[T any] struct {
+		name string
+		arg  struct {
+			fn  func(v T) T
+			arr []T
+		}
+		expect []T
+	}
+	cases := []testCase[int]{
+		{
+			name: "should success",
+			arg: struct {
+				fn  func(v int) int
+				arr []int
+			}{
+				arr: []int{1, 2, 3, 4, 5},
+				fn:  func(v int) int { return v * 2 },
+			},
+			expect: []int{2, 4, 6, 8, 10},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			curriedMap := MapCurry(c.arg.fn)
+
+			actual := curriedMap(c.arg.arr)
+
+			if !reflect.DeepEqual(actual, c.expect) {
+				t.Errorf("expect: %v", c.expect)
+				t.Errorf("actual: %v", actual)
+			}
+		})
+	}
+}
+
+func TestReduce(t *testing.T) {
+	type testCase[T any] struct {
+		name string
+		arg  struct {
+			fn      func(acc, cur T) T
+			arr     []T
+			initial T
+		}
+		expect T
+	}
+
+	cases := []testCase[int]{
+		{
+			name: "should success",
+			arg: struct {
+				fn      func(acc int, cur int) int
+				arr     []int
+				initial int
+			}{
+				fn:      func(acc, cur int) int { return acc + cur },
+				arr:     []int{1, 2, 3, 4},
+				initial: 0,
+			},
+			expect: 10,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := Reduce(c.arg.fn, c.arg.arr, c.arg.initial)
+
+			if !reflect.DeepEqual(actual, c.expect) {
+				t.Errorf("actual: %v", actual)
+				t.Errorf("expect: %v", c.expect)
+			}
+		})
+	}
+}
+
+func TestReduceCurry(t *testing.T) {
+	type testCase[T Generics] struct {
+		name string
+		arg struct{
+			fn func(acc, cur T) T 
+			arr []T
+			initial T
+		}
+		expect T
+	}
+
+	cases := []testCase[int]{
+		{
+			name: "should success",
+			arg: struct{fn func(acc int, cur int) int; arr []int; initial int}{
+				fn: func(acc, cur int) int {return acc+cur},
+				arr: []int{1,2,3,4},
+				initial: 0,
+			},
+			expect: 10,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			reduce := ReduceCurry(c.arg.fn)
+
+			actual := reduce(c.arg.arr, c.arg.initial)
+
+			if !reflect.DeepEqual(actual, c.expect) {
 				t.Errorf("actual: %v", actual)
 				t.Errorf("expect: %v", c.expect)
 			}
